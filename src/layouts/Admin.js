b834e89e7a0797f5516c27d463083830c3209942
arisygdc <arisycso@gmail.com>
2022-04-24 22:27:04 +0700

    base path

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -82,7 +82,7 @@ const Admin = (props) => {
         />
         <Switch>
           {getRoutes(routes)}
-          <Redirect from="*" to="/admin/index" />
+          <Redirect from="*" to="/admin/monitoring-request" />
         </Switch>
         <Container fluid>
           <AdminFooter />
